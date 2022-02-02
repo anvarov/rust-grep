afd64189395d884d5af92b6138106b7434f75098
Akmal Anvarov <akmal@anvarov.dev>
2022-02-01 22:29:14 -0500

    use iterator instead clone

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,23 @@
 use std::env;
-use std::fs;
 use std::process;
 
+use rustgrep::Config;
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
-        process::exit(1)
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing args, {}", err);
+        process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    print!("In file {}", config.filename);
-    
-    let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
-    println!("With text :\n {}", contents);
+    // let args: Vec<String> = env::args().collect();
+    // let config = Config::new(&args).unwrap_or_else(|err| {
+    //     println!("Problem parsing arguments: {}", err);
+    //     process::exit(1)
+    // });
+    println!("Searching for {} \n", config.query);
+    print!("In file {} \n", config.filename);
+    if let Err(e) = rustgrep::run(config) {
+        println!(" Applicatoin Error {}", e);
+        process::exit(1)
+    } 
 }
 
-struct Config {
-    query: String,
-    filename: String,
-}
 
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments!")
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config {query, filename})    
-    }
-}
